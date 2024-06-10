0b47337ba91f4694227f5ccee8d04de0261c7377
fantom4545 <166008514+fantom4545@users.noreply.github.com>
2024-06-10 18:23:23 +0700

    Add files via upload

---
+++ homework15.py
@@ -0,0 +1,12 @@
+class House:
+    def __init__(self):
+        self.numberOfFloors = 0
+
+    def setNewNumberOfFloors(self, floors):
+        self.numberOfFloors = floors
+        print(self.numberOfFloors)
+
+
+House1 = House()
+
+House1.setNewNumberOfFloors(9)
